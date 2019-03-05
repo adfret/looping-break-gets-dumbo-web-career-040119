7aeb12ce2bdec177dc570e04860c214c2a21c007
Ad Friedman <adien330@yahoo.com>
2019-03-05 21:27:15 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
 def levitation_quiz
-	#your code here
+	#your codef annoying
+    loop do
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
 end
-
-
